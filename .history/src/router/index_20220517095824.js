e0cec802897fc068bd9f81e69ed4c3615b8d8af1
liyintao <1293326374@qq.com>
2022-05-17 10:16:37 +0800

    新建登录分支新建登录组件配置路由

--- .history/src/router/index_20220517095824.js
+++ .history/src/router/index_20220517095824.js
@@ -2,8 +2,6 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '@/components/login/login.vue'
 
-Vue.use(Router)
-
 export default new Router({
     routes: [{
         name: 'login',
